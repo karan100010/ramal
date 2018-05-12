7f90eb19be601656d29fd2f1f181cd08839ea401
karan100010 <karanjoshi1993@gmail.com>
2018-05-12 15:01:01 +0530

    changes

--- get_png.py
+++ get_png.py
@@ -11,7 +11,8 @@ from pandas_datareader import data as wb
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 
-d =pd.read_csv(filename)
+def get_png(filename,col,name,t_interval,iterations):
+   d =pd.read_csv(filename)
    d.index=d.Date
    d=d.drop(columns="Date")
    d=d.drop(columns="Volume") 
@@ -22,8 +23,6 @@ d =pd.read_csv(filename)
    stdev= log_return.std()
    drift= u-(0.5*var)
    z=norm.ppf(np.random.rand(10,2))
-   t_interval=t_interval
-   iterations=iterations
    daily_return=np.exp(drift+stdev*norm.ppf(np.random.rand(t_interval,iterations)))
    s0= d[col].iloc[-1]
    price_list= np.zeros_like(daily_return)
@@ -31,4 +30,4 @@ d =pd.read_csv(filename)
    for i in range(1,t_interval):
     price_list[i]=price_list[i-1]*daily_return[i]
    plt.plot(price_list)
-   plt.savefig(name_png)
+   plt.savefig(name)
