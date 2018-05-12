a2edfe78cf2a82e49f011e204b53b69451d0b382
karan100010 <karanjoshi1993@gmail.com>
2018-05-12 14:31:14 +0530

    genrate pngs from dataset

---
+++ get_png.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 12 14:22:41 2018
+
+@author: lilhack110
+"""
+import numpy as np
+import pandas as pd
+from pandas_datareader import data as wb
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+
+d =pd.read_csv(filename)
+   d.index=d.Date
+   d=d.drop(columns="Date")
+   d=d.drop(columns="Volume") 
+   
+   log_return= np.log(1+d[col].pct_change())
+   u= log_return.mean()
+   var= log_return.var()
+   stdev= log_return.std()
+   drift= u-(0.5*var)
+   z=norm.ppf(np.random.rand(10,2))
+   t_interval=t_interval
+   iterations=iterations
+   daily_return=np.exp(drift+stdev*norm.ppf(np.random.rand(t_interval,iterations)))
+   s0= d[col].iloc[-1]
+   price_list= np.zeros_like(daily_return)
+   price_list[0]=s0
+   for i in range(1,t_interval):
+    price_list[i]=price_list[i-1]*daily_return[i]
+   plt.plot(price_list)
+   plt.savefig(name_png)
